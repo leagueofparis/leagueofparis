5dabab47f36afd2d4f8d58f19cb0e25f435e8d18
Brendan <brendan612@gmail.com>
2025-06-10 13:50:40 -0700

    fixed vite config

--- leagueofparis/vite.config.js
+++ leagueofparis/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
@@ -8,14 +9,14 @@ export default defineConfig({
 		proxy: {
 			// Change '/api' to match the path you use for your API requests
 			"/api": {
-				target: import.meta.env.VITE_API_URL, // Replace with your API's port
+				target: process.env.VITE_API_URL, // Replace with your API's port
 				changeOrigin: true,
 				secure: false,
 				// Optional: rewrite the path if your API doesn't have '/api' prefix
 				rewrite: (path) => path.replace(/^\/api/, ""),
 			},
 			"/auth": {
-				target: import.meta.env.VITE_API_URL,
+				target: process.env.VITE_API_URL,
 				changeOrigin: true,
 				secure: false,
 				rewrite: (path) => path.replace(/^\/auth/, "/api/auth"),
